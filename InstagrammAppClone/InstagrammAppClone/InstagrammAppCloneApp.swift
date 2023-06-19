cbd717add899248f8dcdb6735ea3f11a20fa0f19
Ernest Avagovich <erni1937@gmail.com>
2023-06-19 20:54:18 +0300

    init

---
+++ InstagrammAppClone/InstagrammAppClone/InstagrammAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagrammAppCloneApp.swift
+//  InstagrammAppClone
+//
+//  Created by Ernest Avagovich on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagrammAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
