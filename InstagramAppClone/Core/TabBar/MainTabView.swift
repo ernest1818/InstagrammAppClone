e3b8ca5182cef7bc0630d141b5826a64d4f42cbf
Ernest Avagovich <erni1937@gmail.com>
2023-06-19 20:30:53 +0300

    setup TabBarView

---
+++ InstagramAppClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabView.swift
+//  InstagramAppClone
+//
+//  Created by Ernest Avagovich on 19.06.2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("UploadPost")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
